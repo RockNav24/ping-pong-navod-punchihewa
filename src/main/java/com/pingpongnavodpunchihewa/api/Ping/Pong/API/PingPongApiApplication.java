cf90e8de013d5c333c27dc8602c803396e489e4b
RockNav <navodsanthana@gmail.com>
2022-09-29 22:47:17 +0100

    add ping route that returns string pong

--- src/main/java/com/pingpongnavodpunchihewa/api/Ping/Pong/API/PingPongApiApplication.java
+++ src/main/java/com/pingpongnavodpunchihewa/api/Ping/Pong/API/PingPongApiApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PingPongApiApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(PingPongApiApplication.class, args);
 	}
 
