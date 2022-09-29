464753135d8c198d3915afdc28a3aaaf698200fa
RockNav <navodsanthana@gmail.com>
2022-09-29 22:37:10 +0100

    initial commit

---
+++ src/main/java/com/pingpongnavodpunchihewa/api/Ping/Pong/API/PingPongApiApplication.java
@@ -0,0 +1,13 @@
+package com.pingpongnavodpunchihewa.api.Ping.Pong.API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PingPongApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PingPongApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pingpongnavodpunchihewa/api/Ping/Pong/API/PingPongApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pingpongnavodpunchihewa.api.Ping.Pong.API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PingPongApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
